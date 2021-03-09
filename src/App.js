773be811109985134bf4ae0bd358fea845a69c92
Duncan Awerbuck <duk444@gmail.com>
2021-03-09 15:14:06 +0000

    initial POC with aria-live, aria-busy, etc.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./styles.css";
 export default function App() {
   const [loading, setLoading] = useState(true);
   const [hasAnnounced, setHasAnnounced] = useState(false);
+  const [route, setRoute] = useState("/");
 
   const ariaAttributes = {
     "aria-atomic": "true",
@@ -28,15 +29,44 @@ export default function App() {
     };
   }, [loading, hasAnnounced]);
 
-  return (
-    <main {...ariaAttributes}>
-      {loading ? "Loading..." : "loaded"}
-      <button type="button" onClick={() => setLoading(false)}>
-        Stop loading
-      </button>
-      Blah blah blah blah blah.
-      <br />
-      <code aria-hidden="true">{JSON.stringify(ariaAttributes, null, 2)}</code>
-    </main>
-  );
+  useEffect(() => {
+    console.log("focusing on main, the route is", route);
+    document.getElementsByTagName("main")[0].focus();
+  }, [route]);
+
+  console.log("The route is", route);
+
+  return route === "/" ? renderScreen1() : renderScreen2();
+
+  function renderScreen2() {
+    return (
+      <>
+        <div role="status" aria-live="polite">
+          {loading ? "Loading..." : "loaded"}
+        </div>
+
+        <main {...ariaAttributes} tabIndex="-1">
+          <button type="button" onClick={() => setLoading(false)}>
+            Stop loading
+          </button>
+          Blah blah blah blah blah.
+          <p>{loading ? "(i am a stencil" : "(i am the real data!"}</p>
+          <br />
+          <code aria-hidden="true">
+            {JSON.stringify(ariaAttributes, null, 2)}
+          </code>
+        </main>
+      </>
+    );
+  }
+
+  function renderScreen1() {
+    return (
+      <main>
+        <button type="button" onClick={() => setRoute("/screen2")}>
+          Go to screen 2
+        </button>
+      </main>
+    );
+  }
 }
